faf89949fbc17aaee7232d69756035863df4f443
Jaxcoder <codenamejason@outlook.com>
2022-11-03 14:38:51 -0400

    add wallets and siwe

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,6 +1,36 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import "@rainbow-me/rainbowkit/styles.css";
+import { Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+import type { AppProps } from "next/app";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+import "../styles/globals.css";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export const { chains, provider } = configureChains(
+  [chain.goerli],
+  [publicProvider()],
+);
+
+export const { connectors } = getDefaultWallets({
+  appName: "TALENT STAKING",
+  chains,
+});
+
+export const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
+export default function App({ Component, pageProps }: AppProps<{ session: Session }>) {
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <SessionProvider session={pageProps.session} refetchInterval={0}>
+          <Component {...pageProps} />
+        </SessionProvider>
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
