c7a17ee5137474d505d2d6b0635f0cd29c414a88
Jaxcoder <codenamejason@outlook.com>
2022-11-16 21:39:22 -0500

    test hook

--- hardhat/contracts/ governance/GovernorBeta.sol
+++ hardhat/contracts/ governance/GovernorBeta.sol
@@ -1,5 +1,5 @@
-pragma solidity >=0.8.0 <0.9.0;
-//SPDX-License-Identifier: MIT
+pragma solidity ^0.0.15;
+//SPDX-License-Identifier: GPL
 
 import "hardhat/console.sol"; // Remove after testing is completed - JR
 import "@openzeppelin/contracts/access/Ownable.sol";
