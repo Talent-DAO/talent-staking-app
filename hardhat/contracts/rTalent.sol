f35277eca2f1f3f7af9ac99a968f68dd6549d9a3
Jaxcoder <codenamejason@outlook.com>
2022-11-20 20:59:40 -0500

    some more updates

--- hardhat/contracts/rTalent.sol
+++ hardhat/contracts/rTalent.sol
@@ -2,12 +2,45 @@
 // SPDX-License-Identifier: GPL
 pragma solidity ^0.8.15;
 
-import "@openzeppelin/contracts/token/ERC1155/presets/ERC1155PresetMinterPauser.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract rTalent is ERC1155 {
-    constructor() ERC1155("https://api.talentdao.org/api/v1/nft/{id}") {}
+/// @custom:security-contact jaxcoder75@gmail.com
+contract rTalent is ERC1155, Ownable, ERC1155Burnable, ERC1155Supply {
+    constructor(address _owner)
+        ERC1155("https://api.talentdao.org/api/v1/nft/{id}")
+    {
+        transferOwnership(_owner);
+    }
 
-    function mint(address account, uint256 id, uint256 amount, bytes memory data) public {
+    function mint(
+        address account,
+        uint256 id,
+        uint256 amount,
+        bytes memory data
+    ) public {
         _mint(account, id, amount, data);
+    }
+
+    function setURI(string memory newuri) public onlyOwner {
+        _setURI(newuri);
+    }
+
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+        public
+        onlyOwner
+    {
+        _mintBatch(to, ids, amounts, data);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+        internal
+        override(ERC1155, ERC1155Supply)
+    {
+        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
     }
 }
